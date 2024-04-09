8e16618a9633b2a4826fa8415790117fff181bed
unknown <dannysaravia100@gmail.com>
2024-04-08 22:04:06 -0400

    added about and home routes to app.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the content of the home page.</p>
+            <h1>Hello World</h1>
+        <h2>Danny's Bognos</h2>
+        <h1>i am page home</h1>
+        </div>
+    );
+};
+
+export default Home;
