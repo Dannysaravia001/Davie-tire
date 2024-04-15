c1b584dbad4ae5eb957c326cd800559a745293dc
[Luis Saravia] <[lsara011@fiu.edu]>
2024-04-15 19:30:57 -0400

    Added navbar and section title to the page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,6 @@ const Home = () => {
             <h1>Welcome to the Home Page</h1>
             <p>This is the content of the home page.</p>
             <h1>Hello World</h1>
-        <h2>Danny's Bognos</h2>
-        <h1>i am page home</h1>
         </div>
     );
 };
